17e7fcaf9475cd03c0a64e0138a78d74cd0d580f
David Richard <david.richard@acct.global>
2021-09-11 12:30:47 -0300

    create methods http

--- src/index.js
+++ src/index.js
@@ -2,8 +2,26 @@ const express = require('express');
 
 const app = express();
 
-app.get("/", (request, response) => {
-    return response.json({message: 'hello word'});
-})
+app.use(express.json());
+
+app.get("/courses", (request, response) => {
+    return response.json(["curso 1", "curso 2"]);
+});
+
+app.post("/courses", (request, response) => {
+    return response.json(["curso 1", "curso 2", "curso 3"]);
+});
+
+app.put("/courses/:id", (request, response) => {
+    return response.json(["curso 1", "curso 2", "curso 4"]);
+});
+
+app.patch("/courses/:id", (request, response) => {
+    return response.json(["curso 1", "curso 4", "curso 5"]);
+});
+
+app.delete("/courses/:id", (request, response) => {
+    return response.json(["curso 1", "curso 4"]);
+});
 
 app.listen(3333);
